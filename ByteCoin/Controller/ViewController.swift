e090ac781fe823770824ce0d1afedacb6eaafdcf
RubinoPD <robertaskask@gmail.com>
2021-01-22 13:15:14 +0200

    added CoinData and API request

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -9,18 +9,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     let coinManager = CoinManager()
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return coinManager.currencyArray.count
-    }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return coinManager.currencyArray[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let selectedCurrency = coinManager.currencyArray[row]
-        coinManager.getCoinPrice(for: selectedCurrency)
-    }
     
     
     @IBOutlet weak var bitcoinLabel: UILabel!
@@ -34,7 +23,19 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
     }
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
 
 }
 
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -10,6 +10,51 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
+        // Use String concatination to add selected currency at the end of the baseURL along with the URL key.
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
         
+        // Use optional binding to unwrap the URL that is created from the urlString
+        if let url = URL(string: urlString) {
+            
+            // Create a new URLSession object with default configuration
+            let session = URLSession(configuration: .default)
+            
+            // Create a new data task for the URLSession
+            let task = session.dataTask(with: url) {(data, response, error) in
+                if error != nil {
+                print(error!)
+                return
+                }
+                
+                if let safeData = data {
+                    let bitcoinPrice = self.parseJSON(safeData)
+                }
+            }
+            
+            // Start tas to fetch data from bitoin average's server.
+            task.resume()
+        }
     }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        
+        // Create JSON decoder
+        let decoder = JSONDecoder()
+        do {
+            
+            // Try to decode the data using the CoinData structure
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            
+            // Get the last property from the decoded data.
+            let lastPrice = decodedData.rate
+            print(lastPrice)
+            return lastPrice
+        } catch {
+            
+            // Catch any print errors
+            print(error)
+            return nil
+        }
+    }
+    
 }
