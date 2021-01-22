7db9422aff42589eaee97b8b1c70240ccbaa4d84
RubinoPD <robertaskask@gmail.com>
2021-01-22 12:18:11 +0200

    add picker view functionality

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -5,9 +5,11 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "9C4C396F-A800-426E-BC0E-20031EA64779"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
-    
+    func getCoinPrice(for currency: String) {
+        
+    }
 }
